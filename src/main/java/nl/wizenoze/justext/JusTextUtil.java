eae3ec9e231a22e622585c815c52852c3aec824b
Laszlo Csontos <csontos.blaszlo@gmail.com>
2016-01-07 12:22:25 +0100

    Add first source files

---
+++ src/main/java/nl/wizenoze/justext/JusTextUtil.java
@@ -0,0 +1,7 @@
+package nl.wizenoze.justext;
+
+/**
+ * Created by lcsontos on 1/7/16.
+ */
+public class JusTextUtil {
+}
---
+++ src/test/groovy/JusTextUtilTest.groovy
@@ -0,0 +1,7 @@
+import spock.lang.Specification
+
+/**
+ * Created by lcsontos on 1/7/16.
+ */
+class JusTextUtilTest extends Specification {
+}
