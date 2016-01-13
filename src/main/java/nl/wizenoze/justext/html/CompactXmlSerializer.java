9f01c9354cd94d9c6a50313431e3fb2678ae3c55
Laszlo Csontos <csontos.blaszlo@gmail.com>
2016-01-13 14:05:37 +0100

    Source format - fix checkstyle violations

--- src/main/java/nl/wizenoze/justext/html/CompactXmlSerializer.java
+++ src/main/java/nl/wizenoze/justext/html/CompactXmlSerializer.java
@@ -1,13 +1,31 @@
+/**
+ * Copyright (c) 2016-present WizeNoze B.V. All rights reserved.
+ *
+ * This file is part of justext-java.
+ *
+ * justext-java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * justext-java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package nl.wizenoze.justext.html;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 import org.htmlcleaner.BaseToken;
 import org.htmlcleaner.CleanerProperties;
-import org.htmlcleaner.CommentNode;
 import org.htmlcleaner.ContentNode;
 import org.htmlcleaner.TagNode;
 import org.htmlcleaner.XmlSerializer;
@@ -26,22 +44,32 @@ class CompactXmlSerializer extends XmlSerializer {
         serializeOpenTag(tagNode, writer, false);
 
         List<? extends BaseToken> tagChildren = tagNode.getAllChildren();
-        if ( !isMinimizedTagSyntax(tagNode) ) {
-            ListIterator<? extends BaseToken> childrenIt = tagChildren.listIterator();
-            while ( childrenIt.hasNext() ) {
-                Object item = childrenIt.next();
+
+        if (!isMinimizedTagSyntax(tagNode)) {
+            Iterator<? extends BaseToken> childrenIterator = tagChildren.listIterator();
+
+            while (childrenIterator.hasNext()) {
+                Object item = childrenIterator.next();
+
                 if (item != null) {
-                    if ( item instanceof ContentNode) {
+                    if (item instanceof ContentNode) {
                         String content = ((ContentNode) item).getContent();
-                        writer.write( dontEscape(tagNode) ? content.replaceAll("]]>", "]]&gt;") : escapeXml(content) );
+
+                        if (dontEscape(tagNode)) {
+                            content = content.replaceAll("]]>", "]]&gt;");
+                        } else {
+                            content = escapeXml(content);
+                        }
+
+                        writer.write(content);
                     } else if (item instanceof TagNode) {
-                        TagNode tagNodeItem = (TagNode)item;
+                        TagNode tagNodeItem = (TagNode) item;
 
                         if (!"head".equalsIgnoreCase(tagNodeItem.getName())) {
                             tagNodeItem.serialize(this, writer);
                         }
                     } else {
-                        ((BaseToken)item).serialize(this, writer);
+                        ((BaseToken) item).serialize(this, writer);
                     }
                 }
             }
