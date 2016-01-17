835bd502a5ac8d73659aeef21f981a1f5b1f4dc1
Laszlo Csontos <csontos.blaszlo@gmail.com>
2016-01-17 13:13:05 +0100

    Specify stop-words language, otherwise the algorithm results in an empty list

--- src/test/groovy/nl/wizenoze/justext/JusTextTest.groovy
+++ src/test/groovy/nl/wizenoze/justext/JusTextTest.groovy
@@ -21,10 +21,15 @@ class JusTextTest extends Specification {
         String html = loadHtml("cosmos.html")
 
         when:
-        List<Paragraph> paragraphs = jusText.extract(html)
+        List<Paragraph> paragraphs = jusText.extract(html, "en")
 
         then:
-        println(paragraphs.collect { it.text })
+        paragraphs.size() == 5
+        paragraphs[0].text == "We've already covered the universe. Let's focus on something smaller, a galaxy. Right " +
+                "now, you're sitting on a planet that orbits a star in the Milky Way galaxy. As you know, there are " +
+                "loads of galaxies in the universe. Each is different from the other, like fingerprints. We're going " +
+                "to talk about a few of the common traits of galaxies and how you can look up in the sky and look " +
+                "for other galaxies."
     }
 
     /*
@@ -34,10 +39,14 @@ class JusTextTest extends Specification {
         String html = loadHtml("grassland.html")
 
         when:
-        List<Paragraph> paragraphs = jusText.extract(html)
+        List<Paragraph> paragraphs = jusText.extract(html, "en")
 
         then:
-        println(paragraphs.collect { it.text })
+        paragraphs.size() == 1
+        paragraphs[0].text == "Grassland biomes exist throughout the Earth, and in many cases can be vast, expanding " +
+                "across millions of square miles, or kilometers. These biomes are marked by sparse trees and " +
+                "extensive grasses as well as a variety of small and large animals. Some of the largest land animals " +
+                "on Earth live in grasslands, including American bison, elephants, giraffes, and so forth."
     }
 
 }
